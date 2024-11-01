2ede8e8fe41378edcf906b4da890e9317d7ed59d
1ongames <114791859+1ongames@users.noreply.github.com>
2024-11-01 15:28:59 +0900

    코드 추가

--- runing-python/lang.py
+++ runing-python/lang.py
@@ -1,1 +1,26 @@
-# 그냥 처음부터 만들자
+import sys
+from run import Reboot2
+
+class Reboot:
+    def __init__(self):
+        self.variables = {}
+
+    def execute(self, code):
+        for line in code.splitlines():
+            self.process_line(line)
+
+    def lang(code):
+        if '오늘도' in code:
+            return 'import'
+        if '메벤' in code:
+            return 'from'
+        if '신창섭' in code:
+            return 'if'
+        if '김창섭' in code:
+            return 'else'
+        if '신김창섭' in code:
+            return 'elif'
+        if '5천플' in code:
+            return '+'
+        if '5천마' in code:
+            return '-'
