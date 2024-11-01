5d4f184ed14c7bd4b71ad2952ada655f1580265e
1ongames <114791859+1ongames@users.noreply.github.com>
2024-11-01 15:13:26 +0900

    lang.py 업데이트

--- runing-python/lang.py
+++ runing-python/lang.py
@@ -1,18 +1,1 @@
-import sys
-
-class Reboot:
-    def __init__(self):
-        self.data = [0]*256
-
-    def toNumber(self, code):
-        tokens = code.split(' ')
-        result = 1
-        for token in tokens:
-            num = (self.data[token.count('어') - 1] if token.count('어') else 0) + token.count('.') - token.count(',')
-            result *= num
-        return result
-
-    def lang(code):
-        if '오늘도' in code:
-            return 'import'
-        
+# 그냥 처음부터 만들자
